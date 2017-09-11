99abdc05525761fe94ab8a449f564e44488c090d
645528331@qq.com <645528331@qq.com>
2017-09-12 00:06:49 +0800

    add

--- 04更高级/01socket编程/聊天/server.py
+++ 04更高级/01socket编程/聊天/server.py
@@ -5,7 +5,10 @@ server.bind(addr)
 server.listen(5)
 conn,addr = server.accept()
 while 1:
-    data = conn.recv(1024)#如果客户端直接发送一个（空数据，例如不发数据直接回车），这里就是阻塞，不会执行。
+    try:
+        data = conn.recv(1024)#如果客户端直接发送一个（空数据，例如不发数据直接回车），这里就是阻塞，不会执行。
+    except Exception as e:
+        break
     print(".......",str(data,"utf8"))
     if not data:
         conn.close()
